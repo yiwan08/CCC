b051164f0c8093db0ab0b97dee8b2cecbec5ea85
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-02-05 00:06:48 -0500

    Create 21j1.java

---
+++ 21j1.java
@@ -0,0 +1,24 @@
+// Return Pressure = 5 * B - 400
+// Return if above, at, below sea level.
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    int B = myObj.nextInt();
+    int Pressure = B * 5 - 400;
+    System.out.println(Pressure);
+    if (Pressure > 100) {
+      System.out.println(-1);
+    } else if (Pressure == 100) {
+      System.out.println(0);
+    } else {
+      System.out.println(1);
+    }
+  }
+}
+
+>>> 99
+  95
+  1
