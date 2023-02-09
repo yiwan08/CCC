c8541e3425ec857c1ded8e9014278bdc112c5fb9
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-02-08 19:06:38 -0500

    Create Easy.java

---
+++ RV_CC/Week_0/Easy.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main
+{
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		q1();
+	}
+	
+	public static void q1() {
+	    System.out.println("Enter Integer: ");
+	    int userInput = scan.nextInt();
+        for (int j = 0; j < userInput; j++) {
+            for (int k = 0; k < userInput - j - 1; k++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < j + 1; k++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+	}
+}
+
+>>> 3
+     #
+    ##
+   ###
